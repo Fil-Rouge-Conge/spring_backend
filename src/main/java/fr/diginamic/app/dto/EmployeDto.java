51eb88a283f3cbb3869d3a9a1a34ed39d8525c2f
Sionra <71220351+Sionra@users.noreply.github.com>
2025-05-13 12:03:03 +0200

    Adding Employe, Admin, Manager and Departement enum

--- src/main/java/fr/diginamic/app/dto/EmployeDto.java
+++ src/main/java/fr/diginamic/app/dto/EmployeDto.java
@@ -1,9 +1,8 @@
-//package fr.diginamic.app.dto;
-//
-//import lombok.Data;
-//
-//@Data
-//public class EmployeDto extends UserDto {
-//    private float daysoffBalance;
-//    private float emplRttBalance;
-//}
+package fr.diginamic.app.dto;
+
+import fr.diginamic.app.model.User;
+
+public class EmployeDto extends UserDto {
+    private float daysoffBalance;
+    private float emplRttBalance;
+}
--- src/main/java/fr/diginamic/app/model/Manager.java
+++ src/main/java/fr/diginamic/app/model/Manager.java
@@ -1,10 +1,33 @@
-//package fr.diginamic.app.model;
-//
-//import jakarta.persistence.DiscriminatorValue;
-//import jakarta.persistence.Entity;
-//
-//@Entity
-//@DiscriminatorValue("MANAGER")
-//public class Manager extends User {
-//    // Ajouter ici des attributs propres au Manager si besoin.
-//}
+package fr.diginamic.app.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("MANAGER")
+public class Manager extends Employe {
+    // Ajouter ici des attributs propres au Manager si besoin.
+
+    public Manager() {
+        super();
+    }
+
+    public Manager(String lastName, String firstName, String email, String password, Role role, String token, float daysoffBalance, float emplRttBalance){
+        super(lastName, firstName, email, password, role, token, daysoffBalance, emplRttBalance);
+    }
+
+    //TODO
+    public void check_empl_req(){}
+
+    //TODO
+    public void deny_employe_req(){}
+
+    //TODO
+    public void get_histogram_by_month_and_dpt(){}
+
+    //TODO
+    public void export_mana_view_excel(){}
+
+    //TODO
+    public void get_mana_view_by_month_and_dpt(){}
+}
