03097f99ae6b978c3df53cd4fea19c54961ea21e
Sionra <71220351+Sionra@users.noreply.github.com>
2025-05-13 13:23:26 +0200

    adding test data from the rebase

--- src/main/java/fr/diginamic/app/AppApplication.java
+++ src/main/java/fr/diginamic/app/AppApplication.java
@@ -23,6 +23,14 @@ public class AppApplication {
 			User user = new User("Ciel", "Madrigal", "email", "password", Role.EMPLOYE, "token");
 			userRepository.save(user);
 			System.out.println("User inserted!");
+			Employe empl = new Employe("To Heaven", "Stairway", "stairway@toheaven.gg", "heaven", Role.EMPLOYE, "tokenToHeaven", 2, 1);
+			userRepository.save(empl);
+			System.out.println("Employe inserted!");
+			Manager mng = new Manager("Moissa", "Matt", "matt@moissa.gg", "moissa", Role.MANAGER, "MMoaiTSTSa", 10, 1);
+			userRepository.save(mng);
+			System.out.println("Manager inserted!");
+			Admin adm = new Admin("Rouille", "Pat", "pat@rouille.gg", "Patrouille", Role.ADMIN, "PTRLL", 5, 0);
+			userRepository.save(adm);
 
 			DayOff dayOff = new DayOff(LocalDate.of(2025,6,10),LocalDate.of(2025,6,12), "Vacances d'été", Type.PAID_DAY_OFF, Status.INITIAL);
 			dayOffRepository.save(dayOff);
