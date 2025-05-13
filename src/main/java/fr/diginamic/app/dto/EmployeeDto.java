bb25e9db885e1dfded405f80416f0715dde35de0
Sionra <71220351+Sionra@users.noreply.github.com>
2025-05-13 15:37:31 +0200

    Code refactor, merging all User type in one Object

---
+++ src/main/java/fr/diginamic/app/dto/EmployeeDto.java
@@ -0,0 +1,10 @@
+package fr.diginamic.app.dto;
+
+public class EmployeeDto {
+    private Long id;
+    private String lastName;
+    private String firstName;
+    private String email;
+    private float daysoffBalance;
+    private float emplRttBalance;
+}
