d098c405620bec80be26ebc0946c426922aba696
Sionra <71220351+Sionra@users.noreply.github.com>
2025-05-14 12:27:05 +0200

    adding service, and controller for Employee

--- src/main/java/fr/diginamic/app/dto/EmployeeMapper.java
+++ src/main/java/fr/diginamic/app/dto/EmployeeMapper.java
@@ -1,28 +1,34 @@
-//package fr.diginamic.app.dto;
-//
-//import fr.diginamic.app.model.Employe;
-//
-//public class EmployeMapper {
-//
-//    public static EmployeDto toDto(Employe employe) {
-//        EmployeDto dto = new EmployeDto();
-//        dto.setId(employe.getId());
-//        dto.setFirstName(employe.getFirstName());
-//        dto.setLastName(employe.getLastName());
-//        dto.setEmail(employe.getEmail());
-//        dto.setDaysoffBalance(employe.getDaysoffBalance());
-//        dto.setEmplRttBalance(employe.getEmplRttBalance());
-//        return dto;
-//    }
-//
-//    public static Employe toEntity(EmployeDto dto) {
-//        Employe employe = new Employe();
-//        employe.setId(dto.getId());
-//        employe.setFirstName(dto.getFirstName());
-//        employe.setLastName(dto.getLastName());
-//        employe.setEmail(dto.getEmail());
-//        employe.setDaysoffBalance(dto.getDaysoffBalance());
-//        employe.setEmplRttBalance(dto.getEmplRttBalance());
-//        return employe;
-//    }
-//}
+package fr.diginamic.app.dto;
+
+import fr.diginamic.app.model.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto toDto(Employee employee) {
+        EmployeeDto dto = new EmployeeDto();
+        dto.setId(employee.getId());
+        dto.setFirstName(employee.getFirstName());
+        dto.setLastName(employee.getLastName());
+        dto.setEmail(employee.getEmail());
+        dto.setPassword(employee.getPassword());
+        dto.setRole(employee.getRole());
+        dto.setDepartement(employee.getDepartement());
+        dto.setDaysoffBalance(employee.getDaysoffBalance());
+        dto.setEmplRttBalance(employee.getEmplRttBalance());
+        return dto;
+    }
+
+    public static Employee toEntity(EmployeeDto dto) {
+        Employee employee = new Employee();
+        employee.setId(dto.getId());
+        employee.setFirstName(dto.getFirstName());
+        employee.setLastName(dto.getLastName());
+        employee.setEmail(dto.getEmail());
+        employee.setPassword(dto.getPassword());
+        employee.setRole(dto.getRole());
+        employee.setDepartement(dto.getDepartement());
+        employee.setDaysoffBalance(dto.getDaysoffBalance());
+        employee.setEmplRttBalance(dto.getEmplRttBalance());
+        return employee;
+    }
+}
--- src/main/java/fr/diginamic/app/repository/EmployeeRepository.java
+++ src/main/java/fr/diginamic/app/repository/EmployeeRepository.java
@@ -1,12 +1,20 @@
 package fr.diginamic.app.repository;
 
+import fr.diginamic.app.model.Departement;
 import fr.diginamic.app.model.Employee;
+import fr.diginamic.app.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmail(String email);
+    List<Employee> findByLastName(String name);
+    List<Employee> findByFirstName(String name);
+    List<Employee> findByEmailContaining(String email);
+    List<Employee> findByRole(Role role);
+    List<Employee> findByDepartement(Departement departement);
 }
