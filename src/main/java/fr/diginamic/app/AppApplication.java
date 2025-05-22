16c1a336392ae444a629bc22dd9b9eaf880e9712
Ciel <vivpiere@gmail.com>
2025-05-22 11:15:51 +0200

    changed data test

--- src/main/java/fr/diginamic/app/AppApplication.java
+++ src/main/java/fr/diginamic/app/AppApplication.java
@@ -26,36 +26,38 @@ public class AppApplication {
 	@Bean
 	CommandLineRunner run(EmployeeRepository employeeRepository, DayOffRepository dayOffRepository, PersonalDayOffRepository personalDayOffRepository, CommonDayOffRepository commonDayOffRepository) {
 		return args -> {
-			Employee user = new Employee("Ciel", "Madrigal", "ciel.m@gmail.com", encoder.encode("p@ssw0rd"), Role.EMPLOYEE);
-			employeeRepository.save(user);
-			System.out.println("User inserted!");
-			Employee empl = new Employee("To Heaven", "Stairway", "stairway@toheaven.gg", encoder.encode("heaven"), Role.EMPLOYEE,Departement.IT , 2, 1);
-			employeeRepository.save(empl);
-			System.out.println("Employe inserted!");
-			Employee mng = new Employee("Moissa", "Matt", "matt@moissa.gg", encoder.encode("moissa"), Role.MANAGER, Departement.IT, 10, 1);
-			employeeRepository.save(mng);
-			System.out.println("Manager inserted!");
-			Employee adm = new Employee("Rouille", "Pat", "pat@rouille.gg", encoder.encode("Patrouille"), Role.ADMIN, Departement.HR , 5, 0);
-			employeeRepository.save(adm);
-
-			PersonalDayOff personalDayOff1 = new PersonalDayOff(
-					LocalDate.of(2025, 8, 15),LocalDate.of(2025, 8, 15),"Raison perso", Status.INITIAL, PersonalDayOffType.RTT_EMPLOYEE);
-			personalDayOff1.setEmployee(empl);
-			personalDayOffRepository.save(personalDayOff1);
-			System.out.println("PersonalDayOff1 inserted!");
-
-			PersonalDayOff personalDayOff2 = new PersonalDayOff(
-					LocalDate.of(2025, 7, 15),LocalDate.of(2025, 7, 29),"Vacances été", Status.WAITING_FOR_APPROVAL, PersonalDayOffType.PAID_DAY_OFF);
-			personalDayOff2.setEmployee(empl);
-			personalDayOffRepository.save(personalDayOff2);
-			System.out.println("PersonalDayOff2 inserted!");
-
-			CommonDayOff commonDayOff = new CommonDayOff(
-					LocalDate.of(2025, 12, 25),LocalDate.of(2025, 12, 25),"Jour de Noël",Status.APPROVED,"Noël", CommonDayOffType.HOLIDAY);
-			commonDayOffRepository.save(commonDayOff);
-			System.out.println("CommonDayOff inserted!");
-			System.out.println(employeeRepository.findByEmail("matt@moissa.gg"));
-			System.out.println("employee found");
+//			Employee managerIt = new Employee("Ciel", "Madrigal", "coralieruiz.dev@gmail.com", encoder.encode("p@ssw0rd"), Role.MANAGER, Departement.IT, 7, 0);
+//			employeeRepository.save(managerIt);
+//			Employee emplIt1 = new Employee("To Heaven", "Stairway", "stairway@toheaven.gg", encoder.encode("heaven"), Role.EMPLOYEE,Departement.IT , 15, 10);
+//			employeeRepository.save(emplIt1);
+//			Employee emplIt2 = new Employee("Moissa", "Matt", "matt@moissa.gg", encoder.encode("moissa"), Role.EMPLOYEE, Departement.IT, 0, 0);
+//			employeeRepository.save(emplIt2);
+//			Employee adminHr = new Employee("Rouille", "Pat", "pat@rouille.gg", encoder.encode("Patrouille"), Role.ADMIN, Departement.HR , 5, 7);
+//			employeeRepository.save(adminHr);
+//
+//			PersonalDayOff personalDayOff1 = new PersonalDayOff(
+//					LocalDate.of(2025, 8, 15),LocalDate.of(2025, 8, 15),"Raison perso", Status.INITIAL, PersonalDayOffType.RTT_EMPLOYEE);
+//			personalDayOff1.setEmployee(emplIt1);
+//			personalDayOffRepository.save(personalDayOff1);
+//
+//			PersonalDayOff personalDayOff2 = new PersonalDayOff(
+//					LocalDate.of(2025, 7, 15),LocalDate.of(2025, 7, 29),"Vacances été", Status.INITIAL, PersonalDayOffType.PAID_DAY_OFF);
+//			personalDayOff2.setEmployee(emplIt1);
+//			personalDayOffRepository.save(personalDayOff2);
+//
+//			PersonalDayOff personalDayOff3 = new PersonalDayOff(
+//					LocalDate.of(2025, 4, 18),LocalDate.of(2025, 4, 18),"Raison perso", Status.INITIAL, PersonalDayOffType.PAID_DAY_OFF);
+//			personalDayOff3.setEmployee(emplIt2);
+//			personalDayOffRepository.save(personalDayOff3);
+//
+//			PersonalDayOff personalDayOff4 = new PersonalDayOff(
+//					LocalDate.of(2025, 3, 12),LocalDate.of(2025, 3, 12),"Raison perso", Status.WAITING_FOR_APPROVAL, PersonalDayOffType.PAID_DAY_OFF);
+//			personalDayOff4.setEmployee(emplIt2);
+//			personalDayOffRepository.save(personalDayOff4);
+//
+//			CommonDayOff commonDayOff = new CommonDayOff(
+//					LocalDate.of(2025, 12, 25),LocalDate.of(2025, 12, 25),"Jour de Noël",Status.APPROVED,"Noël", CommonDayOffType.HOLIDAY);
+//			commonDayOffRepository.save(commonDayOff);
 		};
 	}
 }
